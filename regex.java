65e212644b40664ce67eaba2e82333d95c4ff1c0
nestorrig <neriga10@hotmail.com>
2023-05-24 15:56:17 -0600

    Clase 27: Java aplicado

--- regex.java
+++ regex.java
@@ -1,17 +1,24 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public  class regex{
 
     public static void main(String[] args){
         String file = "../results.csv";
 
+        Pattern pat = Pattern.compile("^2011\\-.*$");
+
         try{
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
             while((line = br.readLine()) != null){
-                System.out.println(line);
+                Matcher matcher = pat.matcher(line);
+                if (matcher.find()) {
+                    System.out.println(line);
+                }
             }
         }catch(Exception e){
             System.out.println("nope!");
